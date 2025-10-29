8d5c106dd757dc1449eda2582f27df713356413b
saifali-pal <saif.ali@mergestack.com>
2025-10-29 16:58:27 +0500

    boundary-suspense added

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,15 @@
 import LoginForm from "@/components/auth/LoginForm";
 import { Card, CardContent } from "@/components/ui/card";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <Card className="w-full max-w-md p-6">
         <CardContent>
-          <LoginForm />
+          <Suspense fallback={<div>Loading...</div>}>
+            <LoginForm />
+          </Suspense>
         </CardContent>
       </Card>
     </div>
